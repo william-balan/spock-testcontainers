207781a6ee9ec3660819db337fcf88d652dbc2d1
William Machado <william.machado@luizalabs.com>
2021-03-28 20:50:28 -0300

    Added wait-for-it

--- src/test/groovy/AmqpTest.groovy
+++ src/test/groovy/AmqpTest.groovy
@@ -3,33 +3,25 @@ import org.testcontainers.containers.wait.strategy.Wait
 import org.testcontainers.spock.Testcontainers
 import spock.lang.Specification
 
-import java.time.Duration
-
-import static org.awaitility.Awaitility.await
-
 @Testcontainers
 class AmqpTest extends Specification {
 
     DockerComposeContainer dockerComposeContainer = new DockerComposeContainer(new File("./docker-compose.yml"))
             .withExposedService("rabbitmq_1", 5672, Wait.forListeningPort())
+            .waitingFor("consumer_1", Wait.forLogMessage(".*Started AppConsumer.*", 1))
             .withLocalCompose(true)
-
-    static final BODY = "message to broker"
-
-    def setup() {
-        def rabbit = dockerComposeContainer.getContainerByServiceName "rabbitmq_1"
-        rabbit.get().execInContainer "rabbitmqadmin", "publish", "exchange=my-exchange", "routing_key=my-routing-key", "payload=${BODY}"
-    }
 
     def "Testing if consumer receives and prints out the message from queue"() {
 
         setup:
-        await().atMost Duration.ofSeconds(5)
+        def body = "message to broker"
+        def rabbit = dockerComposeContainer.getContainerByServiceName "rabbitmq_1"
+        def consumer = dockerComposeContainer.getContainerByServiceName "consumer_1"
 
         when:
-        def consumer = dockerComposeContainer.getContainerByServiceName "kotlin-consumer_1"
+        rabbit.get().execInContainer "rabbitmqadmin", "publish", "exchange=my-exchange", "routing_key=my-routing-key", "payload=${body}"
 
         then:
-        consumer.get().getLogs().contains BODY
+        consumer.get().getLogs().contains body
     }
 }
